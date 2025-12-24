1170202c60ba82c7e1d172ad43b3e0bfe0984479
kochizufan <kochizufan@gmail.com>
2025-12-24 17:07:53 +0900

    fix: Use root-level demo-sw.js to avoid Service Worker scope restriction

---
+++ demo-sw.js
@@ -0,0 +1,5 @@
+// Demo Service Worker entry point
+import { registerRoute } from 'workbox-routing';
+import { Weiwudi_Internal } from './src/weiwudi_gw_logic.js';
+
+Weiwudi_Internal(registerRoute);
